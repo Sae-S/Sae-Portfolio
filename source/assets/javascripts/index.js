3601a9f99123d54def999fbb27e4ac119d51242a
Sae Shikiji <saeshikiji@gmail.com>
2021-09-10 18:47:57 +0900

    Got basic carosel

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,4 +1,5 @@
 import 'bootstrap';
+import $ from 'jquery';
 
 // Typed text on banner
 import Typed from 'typed.js';
@@ -33,3 +34,31 @@ $(document).ready(function () {
     } // End if
   });
 });
+
+// Carousel
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+window.plusSlides = plusSlides;
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+window.currentSlide = currentSlide;
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+
+}
